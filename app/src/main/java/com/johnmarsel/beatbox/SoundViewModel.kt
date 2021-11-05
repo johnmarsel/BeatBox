b43446130cdf6843f585f0bf5d99c1bed357aef6
johnmarsel <vlad.gorbachyov@gmail.com>
2021-11-06 00:53:44 +0300

    Second commit

---
+++ app/src/main/java/com/johnmarsel/beatbox/SoundViewModel.kt
@@ -0,0 +1,24 @@
+package com.johnmarsel.beatbox
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel(private val beatBox: BeatBox): BaseObservable() {
+
+    fun onButtonClicked() {
+        sound?.let {
+            beatBox.play(it)
+        }
+    }
+
+    var sound: Sound? = null
+        set(sound) {
+            field = sound
+            notifyChange()
+        }
+
+    @get:Bindable
+    val title: String?
+        get() = sound?.name
+
+}
